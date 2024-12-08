c14f5246186f55685170aa624759292183d4ac3e
bhanu <22h51a05k4@cmrcet.ac.in>
2024-12-08 18:40:09 +0530

    first

---
+++ primenumber.java
@@ -0,0 +1,18 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime Numbers from 1 to 100:");
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
